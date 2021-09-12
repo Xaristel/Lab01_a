c2e1017dae941bf0856391b8d291616adcd20b1b
Xaristel <20muxa00@bk.ru>
2021-09-12 20:15:16 +0700

    update #1

--- app/src/main/java/com/example/lab01_a/Functions.java
+++ app/src/main/java/com/example/lab01_a/Functions.java
@@ -1,7 +1,16 @@
 package com.example.lab01_a;
 
+/**
+ * Class with special functions
+ */
 public class Functions
 {
+    /**
+     * The special function for comparing int values
+     * @param firstNumber - The first number for comparing
+     * @param secondNumber - The second number for comparing
+     * @return - Return the lowest number
+     */
     protected int FindMin(int firstNumber, int secondNumber)
     {
         if (secondNumber <= firstNumber)
@@ -14,6 +23,12 @@ public class Functions
         }
     }
 
+    /**
+     * The special function for comparing int values
+     * @param firstNumber - The first number for comparing
+     * @param secondNumber - The second number for comparing
+     * @return - Return the greatest number
+     */
     protected int FindMax(int firstNumber, int secondNumber)
     {
         if (firstNumber >= secondNumber)
---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.lab01_a","l":"ExampleInstrumentedTest"},{"p":"com.example.lab01_a","l":"ExampleUnitTest"},{"p":"com.example.lab01_a","l":"Functions"}]
